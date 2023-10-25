3e2ed91011311820d0419aaae09c5f33f760e438
Isaac Herbozo <isaacherbozo06@gmail.com>
2023-10-25 08:35:30 +0200

    Ejercicio10 con Set

--- src/Prog3/Clase/EjerciciosColecciones/Ejercicio10.java
+++ src/Prog3/Clase/EjerciciosColecciones/Ejercicio10.java
@@ -1,16 +1,20 @@
 package Prog3.Clase.EjerciciosColecciones;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Ejercicio10 {
     public static List<String> eliminarDuplicados(List<String> palabras) {
-        List<String> palabrasSinDuplicados = new ArrayList<>();
-        palabras.forEach(palabra -> {
-            if (!palabrasSinDuplicados.contains(palabra)) {
-                palabrasSinDuplicados.add(palabra);
-            }
-        });
-        return palabrasSinDuplicados;
+//        List<String> palabrasSinDuplicados = new ArrayList<>();
+//        palabras.forEach(palabra -> {
+//            if (!palabrasSinDuplicados.contains(palabra)) {
+//                palabrasSinDuplicados.add(palabra);
+//            }
+//        });
+        Set<String> palabrasSinDuplicados = new HashSet<>(palabras);
+
+        return new ArrayList<>(palabrasSinDuplicados);
     }
 }
