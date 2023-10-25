a87a5a529aa09566b821d83f2d446fe8c218599c
Isaac Herbozo <isaacherbozo06@gmail.com>
2023-10-25 09:40:37 +0200

    Ejercicio14 bonito

--- src/Prog3/Clase/EjerciciosColecciones/Ejercicio14.java
+++ src/Prog3/Clase/EjerciciosColecciones/Ejercicio14.java
@@ -8,12 +8,13 @@ import java.util.Map;
 
 public class Ejercicio14 {
     public static List<Envio> agruparEnvios(List<Producto> productos) {
-        Map<LocalDate, List<Producto>> listaEnviosAgrupados = new HashMap<>();
 
+        Map<LocalDate, List<Producto>> listaEnviosAgrupados = new HashMap<>();
         for (Producto producto:productos) {
             listaEnviosAgrupados.putIfAbsent(producto.getEntrega(), new ArrayList<>());
             listaEnviosAgrupados.get(producto.getEntrega()).add(producto);
         }
+
         List<Envio> envios = new ArrayList<>();
         for (Map.Entry<LocalDate, List<Producto>> e : listaEnviosAgrupados.entrySet()) {
             envios.add(new Envio(e.getKey(), e.getValue()));
