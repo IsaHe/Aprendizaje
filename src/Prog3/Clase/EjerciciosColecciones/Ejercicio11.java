c9a7791c9eaa71414bf72eba12cbc30c48c204ca
Isaac Herbozo <isaacherbozo06@gmail.com>
2023-10-25 08:40:27 +0200

    Ejercicio11 con Set

--- src/Prog3/Clase/EjerciciosColecciones/Ejercicio11.java
+++ src/Prog3/Clase/EjerciciosColecciones/Ejercicio11.java
@@ -1,9 +1,13 @@
 package Prog3.Clase.EjerciciosColecciones;
 
+import java.util.ArrayList;
+import java.util.LinkedHashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Ejercicio11 {
     public static List<String> eliminarDuplicados(List<String> palabras) {
-        return palabras;
+        Set<String> palabrasSinDuplicados = new LinkedHashSet<>(palabras);
+        return new ArrayList<>(palabrasSinDuplicados);
     }
 }
