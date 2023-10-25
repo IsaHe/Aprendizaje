606d6312598f2517e84daf6787b8a12380feaf7d
Isaac Herbozo <isaacherbozo06@gmail.com>
2023-10-25 09:01:46 +0200

    Ejercicio13

--- src/Prog3/Clase/EjerciciosColecciones/Ejercicio13.java
+++ src/Prog3/Clase/EjerciciosColecciones/Ejercicio13.java
@@ -1,9 +1,15 @@
 package Prog3.Clase.EjerciciosColecciones;
 
-import java.util.List;
+import java.util.*;
 
 public class Ejercicio13 {
     public static List<Producto> eliminarDuplicados(List<Producto> productos) {
-        return productos;
+//        Set<Producto> productosSinDuplicados = new HashSet<>(productos);
+//        List<Producto> listaOrdenada = new ArrayList<>(productosSinDuplicados);
+//        listaOrdenada.sort(Comparator.comparing(Producto::getEntrega).reversed());
+
+        Set<Producto> productosSinDuplicadosOrdenados = new TreeSet<>(Comparator.comparing(Producto::getEntrega).reversed());
+        productosSinDuplicadosOrdenados.addAll(productos);
+        return new ArrayList<>(productosSinDuplicadosOrdenados);
     }
 }
