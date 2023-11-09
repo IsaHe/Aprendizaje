cb49c3ccb12bdf51f8320447a644c6578baa69a7
Isaac Esteban <isaacherbozo06@gmail.com>
2023-11-09 19:33:40 +0100

    Preparacion parcial

--- src/Prog3/Clase/EjerciciosColecciones/Ejercicio15.java
+++ src/Prog3/Clase/EjerciciosColecciones/Ejercicio15.java
@@ -1,10 +1,26 @@
 package Prog3.Clase.EjerciciosColecciones;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
 
 public class Ejercicio15 {
     public static List<Envio> agruparEnvios(List<Producto> productos) {
-        return new ArrayList<>();
+
+        Map<LocalDate, List<Producto>> mapaEntregas = new TreeMap<>();
+
+        for(Producto p : productos) {
+            mapaEntregas.putIfAbsent(p.getEntrega(), new ArrayList<>());
+            mapaEntregas.get(p.getEntrega()).add(p);
+        }
+
+        List<Envio> envios = new ArrayList<>();
+        for(Map.Entry<LocalDate, List<Producto>> e : mapaEntregas.entrySet()) {
+            envios.add(new Envio(e.getKey(), e.getValue()));
+        }
+
+        return envios;
     }
 }
--- src/Prog3/Clase/EjerciciosColecciones/Ejercicio16.java
+++ src/Prog3/Clase/EjerciciosColecciones/Ejercicio16.java
@@ -1,17 +1,22 @@
 package Prog3.Clase.EjerciciosColecciones;
 
 import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class Ejercicio16 {
+    Map<LocalDate, List<Producto>> mapaEntregas = new TreeMap<>(Comparator.reverseOrder());
     public Ejercicio16(List<Producto> productos) {
+        for (Producto p : productos) {
+            mapaEntregas.putIfAbsent(p.getEntrega(), new ArrayList<>());
+            mapaEntregas.get(p.getEntrega()).add(p);
+        }
     }
 
-    public List<Producto> obtenerProductos(LocalDate of) {
-        return new ArrayList<>();
+    public List<Producto> obtenerProductos(LocalDate ld) {
+        return mapaEntregas.getOrDefault(ld, Collections.emptyList());
     }
 
     public List<LocalDate> obtenerFechas() {
-        return new ArrayList<>();    }
+        return new ArrayList<>(mapaEntregas.keySet());
+    }
 }
