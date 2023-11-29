5143370f4ae8fdd651774ff5237f1804d02a6e18
Isaac Herbozo <isaacherbozo06@gmail.com>
2023-11-29 09:35:42 +0100

    clase 29/11 2 recursividad

--- src/Prog3/Clase/Recursividad/Ejercicios.java
+++ src/Prog3/Clase/Recursividad/Ejercicios.java
@@ -11,11 +11,29 @@ public class Ejercicios {
 
     private static String imprimirStrBinarios(String str) {
         if (!str.contains("b")) return str;
+        return imprimirStrBinarios(str.replaceFirst("b", "0")) + "\n" + imprimirStrBinarios(str.replaceFirst("b", "1"));
+    }
 
-        String str0 = imprimirStrBinarios(str.replaceFirst("b", "0"));
-        String str1 = imprimirStrBinarios(str.replaceFirst("b", "1"));
-
-        return str0 + "\n" + str1;
+    // Otra forma de hacerlo con un mejor uso de memoria
+    private static void imprimirStrBinarios2(char[] patron, int i) {
+        if (patron.length == 0) return;
+        if (i == patron.length) {
+            System.out.println(patron);
+            return;
+        }
+        if (patron[i] == 'b') {
+            patron[i] = '0';
+            imprimirStrBinarios2(patron, i + 1);
+            patron[i] = '1';
+            imprimirStrBinarios2(patron, i + 1);
+            patron[i] = 'b';
+        } else {
+            imprimirStrBinarios2(patron, i + 1);
+        }
+    }
+    private static int calcularCociente(int dividendo, int divisor) {
+        if (dividendo < divisor) return 0;
+        return 1 + calcularCociente(dividendo - divisor, divisor);
     }
 
     public static void main(String[] args) {
@@ -28,7 +46,17 @@ public class Ejercicios {
 
         System.out.println("------------ Ejercicio 2 ------------");
         System.out.println(imprimirStrBinarios("10b1"));
+        System.out.println();
         System.out.println(imprimirStrBinarios("10bb1"));
-        System.out.println(imprimirStrBinarios("bbbb"));
+        System.out.println();
+        imprimirStrBinarios2("bbbb".toCharArray(), 0);
+
+        System.out.println("------------ Ejercicio 3 ------------");
+        System.out.println(calcularCociente(10, 3));
+        System.out.println(calcularCociente(10, 2));
+        System.out.println(calcularCociente(10, 10));
+        System.out.println(calcularCociente(8, 4));
+        System.out.println(calcularCociente(10, 11));
+        System.out.println(calcularCociente(0, 11));
     }
 }
