5b04f42c778c32c36d237e267ede72f9e5d5e866
Isaac Herbozo <isaacherbozo06@gmail.com>
2023-12-01 09:22:58 +0100

    clase 01/12 recursividad

--- src/Prog3/Clase/Recursividad/Ejercicios.java
+++ src/Prog3/Clase/Recursividad/Ejercicios.java
@@ -36,6 +36,65 @@ public class Ejercicios {
         return 1 + calcularCociente(dividendo - divisor, divisor);
     }
 
+    private static void imprimirPermutacionesRecursivo(String str, String prefijo) {
+        if (str.isEmpty()) {
+            System.out.println(prefijo);
+            return;
+        }
+        for (int i = 0; i < str.length(); i++) {
+            imprimirPermutacionesRecursivo(str.substring(0, i) + str.substring(i + 1), prefijo + str.charAt(i));
+        }
+    }
+
+    private static String invertirString(String str) {
+        if (str.length() == 1) return str;
+        return str.charAt(str.length() - 1) + invertirString(str.substring(0, str.length() - 1));
+    }
+
+    private static String invertirPalabras(String str) {
+        if (str.isEmpty()) return "";
+
+        String[] partes = str.split("[\\W]", 2); // [\\W] es cualquier caracter que no sea una letra o un numero
+        if(partes.length < 2) return partes[0];
+
+        return invertirPalabras(partes[1]) + " " + partes[0];
+    }
+
+    //Método longAHexa que recibe un long y devuelve su conversión a hexadecimal.
+    private static String longAHexa(long num) {
+        if (num < 16) {
+            if (num > 9) {
+                switch ((int) num) {
+                    case 10:
+                        return "A";
+                    case 11:
+                        return "B";
+                    case 12:
+                        return "C";
+                    case 13:
+                        return "D";
+                    case 14:
+                        return "E";
+                    case 15:
+                        return "F";
+                }
+            }
+            return Long.toString(num);
+        }
+        return longAHexa(num / 16) + longAHexa(num % 16);
+    }
+
+    //Método imprimirVariaciones que imprima todos los strings de longitud r obtenidos al tomar r elementos con repetición de un conjunto de n elementos pasado como un array de caracteres. Por ejemplo, si recibe el array {A, B, C} y r = generará, AAA, AAB, AAC, ABA, ABB, . . . CCA, CCB, CCC.
+    private static void imprimirVariaciones(char[] conjunto, int r, String resultado) {
+        if (r == 0) {
+            System.out.println(resultado);
+            return;
+        }
+        for (char c : conjunto) {
+            imprimirVariaciones(conjunto, r - 1, resultado + c);
+        }
+    }
+
     public static void main(String[] args) {
         System.out.println("------------ Ejercicio 1 ------------");
         System.out.println(esPalindromo("anitalavalatina"));
@@ -58,5 +117,23 @@ public class Ejercicios {
         System.out.println(calcularCociente(8, 4));
         System.out.println(calcularCociente(10, 11));
         System.out.println(calcularCociente(0, 11));
+
+        System.out.println("------------ Ejercicio 4 ------------");
+        imprimirPermutacionesRecursivo("ABC", "");
+
+        System.out.println("------------ Ejercicio 5 ------------");
+        System.out.println(invertirString("hola"));
+
+        System.out.println("------------ Ejercicio 6 ------------");
+        System.out.println(invertirPalabras("hola que tal"));
+
+        System.out.println("------------ Ejercicio 7 ------------");
+        System.out.println(longAHexa(10));
+        System.out.println(longAHexa(16));
+        System.out.println(longAHexa(17));
+        System.out.println(longAHexa(255));
+
+        System.out.println("------------ Ejercicio 8 ------------");
+        imprimirVariaciones("ABC".toCharArray(), 3, " ");
     }
 }
